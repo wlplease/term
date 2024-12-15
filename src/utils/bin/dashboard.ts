a240371d282f37c64996ef8d0e70d73377c13af5
mintpic dev <108204467+mintpic@users.noreply.github.com>
2024-12-15 14:29:25 -0500

    Commit new terminal public access

--- src/utils/bin/dashboard.ts
+++ src/utils/bin/dashboard.ts
@@ -20,7 +20,8 @@ const dashboard = async (args: string[]): Promise<string> => {
         @@@                  @@@ @@          DONATE 
          @|  @@@@@@@@@@@@@@@@   @@           <u><a href="${config.donate_urls.paypal || '#'}" target="_blank" rel="noopener noreferrer">${config.donate_urls.paypal || '#'}</a></u>
          @|                      @@          <u><a href="${config.donate_urls.patreon || '#'}" target="_blank" rel="noopener noreferrer">${config.donate_urls.patreon || '#'}</a></u>
-
+         @|                      @@          CashApp: <u><a href="https://cash.app/${config.donate_urls.cashapp}" target="_blank" rel="noopener noreferrer">${config.donate_urls.cashapp}</a></u>
+         @|                      @@          Ethereum: <u><a href="https://etherscan.io/address/${config.donate_urls.ethereum}" target="_blank" rel="noopener noreferrer">${config.donate_urls.ethereum}</a></u>
 `;
   } else {
     // Default ASCII Art Version
@@ -32,15 +33,16 @@ const dashboard = async (args: string[]): Promise<string> => {
    ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url || '#'}" target="_blank" rel="noopener noreferrer">resume</a></u>
 ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo || '#'}" target="_blank" rel="noopener noreferrer">Github repo</a></u>
 ▐▓                                 ▐▓       -----------
-▐▓        > NULLSHIFT       ▐▓        CONTACT 
+▐▓        > NULLSHIFT             ▐▓        CONTACT
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email || 'info@nullshift.xyz'}" target="_blank" rel="noopener noreferrer">${config.email || 'info@nullshift.xyz'}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github || ''}" target="_blank" rel="noopener noreferrer">github.com/${config.social.github || ''}</a></u>
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin || ''}" target="_blank" rel="noopener noreferrer">linkedin.com/in/${config.social.linkedin || ''}</a></u>
     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE 
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE
         ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="${config.donate_urls.paypal || '#'}" target="_blank" rel="noopener noreferrer">${config.donate_urls.paypal || '#'}</a></u>
             ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="${config.donate_urls.patreon || '#'}" target="_blank" rel="noopener noreferrer">${config.donate_urls.patreon || '#'}</a></u>
-
+                                              CashApp: <u><a href="https://cash.app/${config.donate_urls.cashapp}" target="_blank" rel="noopener noreferrer">${config.donate_urls.cashapp}</a></u>
+                                              Ethereum: <u><a href="https://etherscan.io/address/${config.donate_urls.ethereum}" target="_blank" rel="noopener noreferrer">${config.donate_urls.ethereum}</a></u>
 `;
   }
 };
